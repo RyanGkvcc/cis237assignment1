bb4353b01f1062c691306275825d6648339b2a25
Ryan Gowan <tr_jester@hotmail.com>
2016-01-23 12:50:48 -0500

    Start WineItems

--- assignment1/WineItem.cs
+++ assignment1/WineItem.cs
@@ -8,5 +8,36 @@ namespace assignment1
 {
     class WineItem
     {
+        private String id;
+        private String description;
+        private String pack;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value; }
+        }
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+
+        public String Pack
+        {
+            get { return pack; }
+            set { pack = value; }
+        }
+
+        public String FullDetails()
+        {
+            return this.id + " " + this.description + " " + this.pack;
+        }
     }
+
+    //public override String ToString()
+    //{
+    //    return this.id + " " + this.description + " " + this.pack;
+    //}
 }
