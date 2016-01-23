bc3f946f1a7990a64964ea6ec916f3196d4c489c
Ryan Gowan <tr_jester@hotmail.com>
2016-01-23 02:12:18 -0500

    Basic set up

---
+++ assignment1/CSVProcessor.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace assignment1
+{
+    class CSVProcessor
+    {
+        public static void Processor()
+        {
+            Int32 arrayLength_Int32;
+
+            try
+            {
+                StreamReader wineList = new StreamReader("WineList.csv");
+                arrayLength_Int32 = CountLinesInFile("WineList.csv");
+                String[] productNumber_String = new String[arrayLength_Int32];
+                String[] wineNames_String = new String[arrayLength_Int32];
+                String[] containerSize_String = new String[arrayLength_Int32];
+                ReadFile(wineList, productNumber_String, wineNames_String, containerSize_String);
+            }
+            catch
+            {
+                Console.Clear();
+                Console.WriteLine("There was a error while reading the file.");
+                MainProgram.Pause();
+            }
+            
+        }
+
+        static int CountLinesInFile(String csv)
+        {
+            int count = 0;
+
+            using (StreamReader wineList = new StreamReader(csv))
+            {
+                String line;
+                while ((line = wineList.ReadLine()) != null)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+
+        private static void ReadFile(StreamReader record, String[] product, String[] names, String[] size)
+        {
+            Int32 counter_Int32 = 0;
+
+            while (!record.EndOfStream)
+            {
+                String[] tempArray_String = record.ReadLine().Split(',');
+                product[counter_Int32] = tempArray_String[0];
+                names[counter_Int32] = tempArray_String[1];
+                size[counter_Int32] = tempArray_String[2];
+                counter_Int32++;
+            }
+            record.Close();
+        }
+    }
+}
---
+++ assignment1/MainProgram.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.IO;
+
+namespace assignment1
+{
+    class MainProgram
+    {
+
+        static void Main(string[] args)
+        {
+            CSVProcessor.Processor();
+        }
+
+        public static void Pause()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Press any key to continue.");
+            Console.ReadKey();
+        }
+    }
+}
